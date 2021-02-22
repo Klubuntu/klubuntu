f5c9ecef09489d39688a5788ae5a32c8989ec9c2
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2021-02-22 15:14:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<head>
+<script data-ad-client="ca-pub-3902832126015588" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+
+<meta property="fb:pages" content="112026313678856" />
+<!-- start webpushr code --> <script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));webpushr('setup',{'key':'BEutarJakXG-GEq5NmDpbiUEEKqPXRTj7zlmlBK1zuK3TI74qS1RPW58bJ2EsC5n-qizRADcYdXzUmIERjQQZ1c' });</script><!-- end webpushr code -->
+<!-- <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script> -->
+<!-- <script>
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "fa65b9f1-08a8-40cc-8fd3-3f540931a3c6",
+    });
+  });
+</script>
+-->
+<!-- PushAlert 
+        <script type="text/javascript">
+                (function(d, t) {
+                        var g = d.createElement(t),
+                        s = d.getElementsByTagName(t)[0];
+                        g.src = "https://cdn.pushalert.co/integrate_49fce05a51f8e017cf758c5f4247e669.js";
+                        s.parentNode.insertBefore(g, s);
+                }(document, "script"));
+        </script> -->
+        <!-- End PushAlert -->
+    <script type="application/javascript" src="https://sdki.truepush.com/sdk/v2.0.2/app.js" async></script>
+</head>
+
+<h2>Zmiana Terminu Otwarcia</h2>
+<p>3 March 2021 y.</p
+<p>Start Beta Tests 25 February 2021 y.</p>
